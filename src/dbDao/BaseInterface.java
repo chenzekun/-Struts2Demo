8ef0853ff2bc4f023c9e361fadf71dbda69a2938
chenzekun <69577932@qq.com>
2018-09-16 01:41:25 +0800

    666

--- src/dbDao/BaseInterface.java
+++ src/dbDao/BaseInterface.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 
 
 public interface BaseInterface {
+<<<<<<< HEAD
 	//测试5555555555  git
+=======
+	//测试3333333333333333  git
+>>>>>>> branch 'master' of https://github.com/chenzekun/-Struts2Demo
 	public boolean Insert(String sql, Object[] arr);
 
 	public boolean Delete(String sql, Object[] arr);
