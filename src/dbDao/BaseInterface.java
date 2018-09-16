fbabd150c687258a5091e4978edcbac5c61832da
chenzekun <69577932@qq.com>
2018-09-17 00:14:49 +0800

    正式测试 3

--- src/dbDao/BaseInterface.java
+++ src/dbDao/BaseInterface.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 
 public interface BaseInterface {
-// 正式测试 222222
+// 正式测试 3333
 	public boolean Insert(String sql, Object[] arr);
 
 	public boolean Delete(String sql, Object[] arr);
