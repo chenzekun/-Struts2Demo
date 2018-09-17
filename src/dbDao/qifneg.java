45ff2e78be4de69fcf3fb1c10408595dcf21c819
Ryan <Ryan@USER-20140921QC>
2018-09-17 17:43:47 +0800

    增加  类

---
+++ src/dbDao/qifneg.java
@@ -0,0 +1,19 @@
+/**   
+ * Copyright © 2018 传一科技.
+ * 
+ * @Package: dbDao 
+ * @author: JX180301_赖麒峰(Ryan)   
+ * @date: 2018年9月17日 下午5:42:27 
+ */
+package dbDao;
+
+/** 
+ * @ClassName: qifneg 
+ * @Description: TODO
+ * @author: JX180301_赖麒峰(Ryan)  
+ * @date: 2018年9月17日 下午5:42:27  
+ */
+public class qifneg
+{
+
+}
