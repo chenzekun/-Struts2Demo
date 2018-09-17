df77fd2ebdf1246d97cbe8b8596a4ef417b5be04
chenzekun <69577932@qq.com>
2018-09-17 16:02:00 +0800

    新新新新 测试 1111

--- src/dbDao/BaseInterface.java
+++ src/dbDao/BaseInterface.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 
 public interface BaseInterface {
-// 正式测试 3333
+// 正式测试 新新新新新 111111111111111
 	public boolean Insert(String sql, Object[] arr);
 
 	public boolean Delete(String sql, Object[] arr);
